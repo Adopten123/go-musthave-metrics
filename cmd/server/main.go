cc84e3f13b2ae00efd84ff6573bd653c24b36f78
Adopten123 <alexandermikhailov0505@gmail.com>
2026-04-29 13:04:22 +0300

    1st commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,53 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/Adopten123/go-musthave-metrics/internal/storage"
+)
+
+func main() {
+	s := storage.NewMemStorage()
+
+	http.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 5 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		mType, mName, mValue := parts[2], parts[3], parts[4]
+
+		switch mType {
+		case "counter":
+			v, err := strconv.ParseInt(mValue, 10, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			s.UpdateCounter(mName, v)
+		case "gauge":
+			v, err := strconv.ParseFloat(mValue, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			s.UpdateGauge(mName, v)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
