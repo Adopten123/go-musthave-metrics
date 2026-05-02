1a5a09b9bfb10acac5160c8b671cbe8b64a757d1
Adopten123 <alexandermikhailov0505@gmail.com>
2026-05-02 20:41:40 +0300

    feat: add environment variables support (iter5)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/Adopten123/go-musthave-metrics/internal/agent"
@@ -16,6 +18,20 @@ func main() {
 	flag.IntVar(&reportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&pollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
+
+	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
+		serverAddr = envAddr
+	}
+	if envReport := os.Getenv("REPORT_INTERVAL"); envReport != "" {
+		if val, err := strconv.Atoi(envReport); err == nil {
+			reportInterval = val
+		}
+	}
+	if envPoll := os.Getenv("POLL_INTERVAL"); envPoll != "" {
+		if val, err := strconv.Atoi(envPoll); err == nil {
+			pollInterval = val
+		}
+	}
 
 	pollTicker := time.NewTicker(time.Duration(pollInterval) * time.Second)
 	reportTicker := time.NewTicker(time.Duration(reportInterval) * time.Second)
--- internal/handler/update.go
+++ internal/handler/update.go
@@ -4,41 +4,36 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Adopten123/go-musthave-metrics/internal/storage"
 	"github.com/go-chi/chi/v5"
 )
 
-func UpdateHandler(s MetricStorage) http.HandlerFunc {
+func UpdateHandler(s *storage.MemStorage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		mType := chi.URLParam(r, "type")
 		mName := chi.URLParam(r, "name")
-		mValue := chi.URLParam(r, "value")
-
-		if mName == "" || mValue == "" {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+		mValueStr := chi.URLParam(r, "value")
 
 		switch mType {
-		case "counter":
-			v, err := strconv.ParseInt(mValue, 10, 64)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			s.UpdateCounter(mName, v)
 		case "gauge":
-			v, err := strconv.ParseFloat(mValue, 64)
+			val, err := strconv.ParseFloat(mValueStr, 64)
 			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
+				http.Error(w, "Bad Request", http.StatusBadRequest)
 				return
 			}
-			s.UpdateGauge(mName, v)
+			s.UpdateGauge(mName, val)
+		case "counter":
+			val, err := strconv.ParseInt(mValueStr, 10, 64)
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+			s.UpdateCounter(mName, val)
 		default:
-			w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "Bad Request: unknown type", http.StatusBadRequest)
 			return
 		}
 
-		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 	}
 }
